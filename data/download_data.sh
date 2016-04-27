355039941e5315c809ff8f0a702bb277c333b959
Yusuke Niitani <yuyuniitani@gmail.com>
2016-04-27 19:11:33 +0900

    Simplify donwload url

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # download cache
-gdown "https://drive.google.com/uc?export=download&id=0BzBTxmVQJTrGR3hDenk2LXNWa00" -O cache.zip
+gdown "https://drive.google.com/uc?id=0BzBTxmVQJTrGR3hDenk2LXNWa00" -O cache.zip
 unzip cache.zip 
 rm cache.zip
 
 # download raw_data 
-gdown "https://drive.google.com/uc?export=download&id=0BzBTxmVQJTrGOEppMVJNeTdsVk0" -O raw_data.zip
+gdown "https://drive.google.com/uc?id=0BzBTxmVQJTrGOEppMVJNeTdsVk0" -O raw_data.zip
 unzip raw_data.zip -d raw_data
 rm raw_data.zip
